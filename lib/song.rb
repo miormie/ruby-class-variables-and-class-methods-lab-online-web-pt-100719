99e2ab0058acc82e0e285d935d4015a0cf6af6d5
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-29 00:57:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :name, :artist and :genre 
+  def initialize(name, artist, genre)
+    @@count += 1 
+    
+    @name = name 
+    @artist = artist
+    @genre = genre
+  end 
+  @@count = 0 
+  
+  def count
+    @@count
+  end 
+  def genre 
+  
+  if @@genres.include? (genre)
+    @@genres << genre  
+  else
+    @@genres 
+  end 
+  end 
+  @@genres = []
+end 
